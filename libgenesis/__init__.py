09a157bf0f020e9328f6f4959ffc263bebdb51ca
Samfun75 <38332931+Samfun75@users.noreply.github.com>
2022-04-14 16:28:10 +0300

    Bump version to 0.1.9

--- libgenesis/__init__.py
+++ libgenesis/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from .search import Libgen
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
